a3b7acca38c264b21317d904105277a5e8f07362
Yusuf Güngör <yusufgngor@gmail.com>
2021-10-23 14:32:04 +0300

    Initial Commit

---
+++ WheatherCaseApp/WheatherCaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherCaseAppApp.swift
+//  WheatherCaseApp
+//
+//  Created by Yusuf Güngör on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct WheatherCaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
